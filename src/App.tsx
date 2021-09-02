19d4036dc82614f120e96d3ff9517ea5fa3fdd25
Freud <alexandromunera@gmail.com>
2021-09-02 09:39:05 -0500

    Buttom atom first version

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,51 @@
 import React from 'react'
+import { Button } from './components/atoms/Button/Button'
 
 function App () {
   return (
-    <h1>Tutti Stop App</h1>
+    <>
+      <h1>Tutti Stop App</h1>
+
+      <h2>Button Atom</h2>
+
+      <Button
+        type="button"
+        onClick={ () => console.log('Di click')}
+        theme="default"
+        size="small"
+      >
+        Prueba
+      </Button>
+
+      <Button
+        type="submit"
+        onClick={ () => console.log('Di click')}
+        theme="rounded"
+        size="medium"
+        disabled
+      >
+        Prueba 2
+      </Button>
+
+      <Button
+        type="reset"
+        onClick={ () => console.log('Di click')}
+        theme="default"
+        size="large"
+      >
+        Prueba 3
+      </Button>
+
+      <Button
+        type="button"
+        onClick={ () => console.log('Di click')}
+        theme="default"
+        size="small"
+        disabled
+      >
+        Prueba 4
+      </Button>
+    </>
   )
 }
 
