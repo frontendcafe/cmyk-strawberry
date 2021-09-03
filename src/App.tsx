6588ef8b7558688c33db38ad01cfc1fe75a63ed6
Freud <alexandromunera@gmail.com>
2021-09-02 22:39:10 -0500

    Change naming component and fix optiones in type prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,8 @@
-import React, { useState } from 'react'
-import { Category } from './components/atoms/Category/Category'
+import React from 'react'
 
 function App () {
-  const [category, setCategory] = useState('deselected')
-  const [category2, setCategory2] = useState('disapproved')
-
-  const handleClickCategory = () => {
-    setCategory(category === 'deselected' ? 'approved' : 'deselected')
-  }
-
-  const handleClickCategory2 = () => {
-    setCategory2(category2 === 'disapproved' ? 'approved' : 'disapproved')
-  }
-
   return (
-    <>
-      <h1>Tutti Stop App</h1>
-
-      <h2>Category Atom</h2>
-
-      <Category
-        type = { category }
-        label="Frutas/Verduras"
-        onClick = { handleClickCategory }
-      />
-
-      <Category
-        type = { category2 }
-        label="Frutas/Verduras"
-        onClick = { handleClickCategory2 }
-      />
-
-    </>
-
+    <h1>Tutti Stop App</h1>
   )
 }
 
