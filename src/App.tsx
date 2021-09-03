ab77644d41f87eec17e139ae8f89389ece6e866b
Freud <alexandromunera@gmail.com>
2021-09-02 22:49:18 -0500

    Change file naming and delete color in .scss file

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,8 @@
 import React from 'react'
-import { Button } from './components/atoms/Button/Button'
 
 function App () {
   return (
-    <>
-      <h1>Tutti Stop App</h1>
-
-      <h2>Atoms</h2>
-
-      <div>
-        <h2>Button</h2>
-
-        <Button
-          type="button"
-          onClick={ () => console.log('Di click')}
-          theme="primary"
-          size="medium"
-        >
-          primary medium
-        </Button>
-        <br/>
-        <Button
-          type="button"
-          onClick={ () => console.log('Di click')}
-          theme="primary"
-          size="medium"
-        >
-          primary small
-        </Button>
-        <br/>
-        <Button
-          type="button"
-          onClick={ () => console.log('Di click')}
-          theme="secondary"
-          size="small"
-        >
-          secondary small
-        </Button>
-        <br/>
-
-        <Button
-          type="button"
-          onClick={ () => console.log('Di click')}
-          theme="secondary"
-          size="medium"
-        >
-          secondary medium
-        </Button>
-        <br/>
-
-        <Button
-          type="button"
-          onClick={ () => console.log('Di click')}
-          theme="tertiary"
-          size="small"
-        >
-          tertiary small
-        </Button>
-        <br/>
-        <Button
-          type="button"
-          onClick={ () => console.log('Di click')}
-          theme="tertiary"
-          size="medium"
-        >
-          tertiary medium
-        </Button>
-
-      </div>
-
-    </>
+    <h1>Tutti Stop App</h1>
   )
 }
 
