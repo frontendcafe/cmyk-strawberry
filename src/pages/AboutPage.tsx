c72bd0ea6a4f7e1f5e021c2c079053e3433352bc
Freud <alexandromunera@gmail.com>
2021-09-03 12:31:45 -0500

    Initial router

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react'
+import { Link, RouteComponentProps, withRouter } from 'react-router-dom'
+
+const AboutPage: React.FC<any & RouteComponentProps<any>> = props => {
+  const [message, setMessage] = useState<string>('')
+
+  useEffect(() => {
+    const number = props.match.params.number
+
+    if (number) {
+      setMessage(`El parámetro es ${number}`)
+    } else {
+      setMessage('Sin parámetro!')
+    }
+  }, [props])
+
+  return (
+    <div>
+      <p>{message}</p>
+      <Link to="/">Ir a home page!</Link>
+    </div>
+  )
+}
+
+export default withRouter(AboutPage)
