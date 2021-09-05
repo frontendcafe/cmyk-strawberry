dbb77357cd68c71ba283784ac757bec93d235e11
Kevin Dev <kevin.developer71@gmail.com>
2021-09-04 21:30:34 -0600

    molecule: slider

--- src/components/molecules/Slider/index.tsx
+++ src/components/molecules/Slider/index.tsx
@@ -1,30 +1,73 @@
 import React, { useState } from 'react'
+import { Range, getTrackBackground } from 'react-range'
+
 import styles from './Slider.module.scss'
 
 interface Props {
-  min?: string
-  max?: string
-  className?: string
+  min?: number
+  max?: number
+  step?: number
 }
 
-const Slider: React.FC<Props> = ({ min = 30, max = 90 }) => {
-  const [value, setValue] = useState(min)
-
-  const handleChange = (e : React.ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value)
-  }
+const Slider: React.FC<Props> = ({ min = 30, max = 90, step = 10 }) => {
+  const [values, setValues] = useState([min])
+  const labels = [30, 40, 50, 60, 70, 80, 90]
 
   return (
     <div className={styles.container}>
       <header className={styles.header}><h3>Segundos</h3></header>
+      <section className={styles.labels}>
+        {labels.map(label => {
+          return (
+            <span
+              key={label}
+              className={`${styles.label} ${label === values[0] && styles.active}`}
+            >
+              {label}
+            </span>
+          )
+        })}
+      </section>
       <div className={styles.slider}>
-        <input
-          className={styles.input}
-          type='range'
+        <Range
+          values={values}
+          step={step}
           min={min}
           max={max}
-          value={value}
-          onChange={(e) => handleChange(e)}
+          onChange={(values) => setValues(values)}
+          renderTrack={({ props, children }) => (
+            <div
+              onMouseDown={props.onMouseDown}
+              onTouchStart={props.onTouchStart}
+              className={styles.track}
+              style={{
+                ...props.style
+              }}
+            >
+              <div
+                ref={props.ref}
+                className={styles.progress}
+                style={{
+                  background: getTrackBackground({
+                    values: values,
+                    colors: [styles['primary-color'], styles['neutral-color']],
+                    min: min,
+                    max: max
+                  })
+                }}
+              >
+                {children}
+              </div>
+            </div>
+          )}
+          renderThumb={({ props }) => (
+            <div
+              {...props}
+              className={styles.thumb}
+            >
+              <div/>
+            </div>
+          )}
         />
       </div>
     </div>
