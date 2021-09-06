fec6f1bc3d9a57def8e2577a475154d6862aa622
Freud <alexandromunera@gmail.com>
2021-09-06 12:40:54 -0500

    Modal atom

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
-import Slider from '../components/molecules/Slider'
+import React, { useEffect } from 'react'
+import { Modal } from '../components/atoms/Modal'
 
 const HomePage: React.FC<any> = () => {
-  return <Slider/>
+  useEffect(() => {
+    Modal.fire({
+      text: 'Volver al inicio del juego',
+      title: '¿Estas seguro que quieres salir de la sala?',
+      confirmButtonText: 'Salir',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        console.log('click in confirm')
+      } else if (result.isDismissed) {
+        console.log('click in cancel')
+      }
+    })
+  }, [])
+
+  return <h1>Home page !</h1>
 }
 
 export default HomePage
