d32d70f62eff42b739e5073161193890c8b7bd80
Freud <alexandromunera@gmail.com>
2021-09-06 12:48:52 -0500

    HomePage like main branch

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,23 +1,8 @@
-import React, { useEffect } from 'react'
-import { Modal } from '../components/atoms/Modal'
+import React from 'react'
+import Slider from '../components/molecules/Slider'
 
 const HomePage: React.FC<any> = () => {
-  useEffect(() => {
-    Modal.fire({
-      text: 'Volver al inicio del juego',
-      title: '¿Estas seguro que quieres salir de la sala?',
-      confirmButtonText: 'Salir',
-      cancelButtonText: 'Cancelar'
-    }).then((result) => {
-      if (result.isConfirmed) {
-        console.log('click in confirm')
-      } else if (result.isDismissed) {
-        console.log('click in cancel')
-      }
-    })
-  }, [])
-
-  return <h1>Home page !</h1>
+  return <Slider/>
 }
 
 export default HomePage
