5fb74ea6f5f2d5e9dcca35d72d7054e6391a9ecf
Agustin Vazquez <agustinvazquez453@gmail.com>
2021-09-14 19:17:27 -0300

    send local room data to firebase

--- src/contexts/RoomContextState.tsx
+++ src/contexts/RoomContextState.tsx
@@ -1,19 +1,30 @@
 import React, { createContext, useState, useEffect } from 'react'
 import useLocalStorage from '../hooks/useLocalStorage'
-import { getRoomByKeyWithSync } from '../firebase/services/room'
+import { getRoomByKeyWithSync, updateRoom } from '../firebase/services/room'
 import { IRoomContext } from '../types/room'
+import { Unsubscribe } from 'firebase/database'
 
 export const RoomContext = createContext<IRoomContext>({} as IRoomContext)
 
 export const RoomProvider: React.FC = ({ children }) => {
   const [room, setRoom] = useState(null)
   const [roomKey] = useLocalStorage('room_key', '-MjKz1_1N27ZUvGxfVrA')
+  let unsuscribeEvent: Unsubscribe | null = null
 
   useEffect(() => {
     if (roomKey) {
-      getRoomByKeyWithSync(roomKey, setRoom)
-    } // TODO: Write to localstorage when join or create a room
-  }, [])
+      if (room) {
+        updateRoom(roomKey, room)
+      } else {
+        unsuscribeEvent = getRoomByKeyWithSync(roomKey, setRoom)
+      }
+    } else {
+      setRoom(null)
+      unsuscribeEvent?.()
+    }
+
+    return () => unsuscribeEvent?.()
+  }, [room, roomKey])
 
   return (
     <RoomContext.Provider value={ { room } }>
