d97b80c66293cd5808dcc6bd51b5151340ae1122
Kevin Dev <kevin.developer71@gmail.com>
2021-09-15 15:23:25 -0600

    feat: rooms integration with preview

--- src/components/Organisms/Rooms/index.tsx
+++ src/components/Organisms/Rooms/index.tsx
@@ -2,28 +2,31 @@ import React from 'react'
 import Room from '../../molecules/Room'
 import SkeletonRoom from '../../molecules/SkeletonRoom'
 import { ReactComponent as EmptyStateIcon } from '../../../assets/empty-state.svg'
+import { IRoom } from '../../../types/room'
 
 import styles from './Rooms.module.scss'
 
 type Props = {
-  rooms: {label: string, rounds: number, totalRounds: number, players: number, private: boolean}[]
+  rooms: IRoom[]
   showPrivate: boolean
 }
 
 const Rooms: React.FC<Props> = ({ rooms, showPrivate }) => {
-  return rooms.length > 0
-    ? (
-      <section className={styles.container}>
-        {rooms.filter(room => room?.private === showPrivate).map((room, idx) => {
-          return <Room key={idx} room={room}/>
-        })}
-        <SkeletonRoom/>
-        <SkeletonRoom/>
-        <SkeletonRoom/>
-        <SkeletonRoom/>
-      </section>
-    )
-    : <EmptyStateIcon className={styles['empty-state']}/>
+  const filteredRooms =
+    showPrivate
+      ? rooms.filter(room => room.password)
+      : rooms.filter(room => !Object.prototype.hasOwnProperty.call(room, 'password'))
+
+  return (
+    <section className={styles.container}>
+      {
+        filteredRooms.length > 0
+          ? filteredRooms.map(room => <Room key={room.id} room={room}/>)
+          : <EmptyStateIcon className={styles['empty-state']}/>
+      }
+      <SkeletonRoom/>
+    </section>
+  )
 }
 
 export default Rooms
