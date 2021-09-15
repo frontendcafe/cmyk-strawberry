57305826f5062b83118780ac60053bef88c045d6
Agustin Vazquez <agustinvazquez453@gmail.com>
2021-09-14 22:13:38 -0300

    add png and jpg types

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,16 @@
 
 declare module '*.scss';
 
+declare module '*.png' {
+  const value: any
+  export = value;
+}
+
+declare module '*.jpg' {
+  const value: any
+  export = value;
+}
+
 declare module '*.svg' {
   import React = require('react');
   export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>
