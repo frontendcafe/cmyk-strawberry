2c5e16048185f70a95f823dcc86d5cfa606d8636
Agustin Vazquez <agustinvazquez453@gmail.com>
2021-09-14 21:30:31 -0300

    integrate useForm with GameConfig

--- src/components/atoms/input/index.tsx
+++ src/components/atoms/input/index.tsx
@@ -2,21 +2,36 @@ import React, { Fragment } from 'react'
 import styles from './Index.module.scss'
 
 interface Props {
-    changeHandler: (value:string) => void;
+    changeHandler?: (value:string) => void;
     buttonHandler?: () => void;
-    size: 'small-size' | 'medium-size'
-    addButton?: boolean
+    size: 'small-size' | 'medium-size';
+    addButton?: boolean;
+    name?: string;
+    value?: string;
+    onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-const Input: React.FC<Props> = ({ changeHandler, buttonHandler, size, addButton = false }) => {
+const Input: React.FC<Props> = ({ name, value, changeHandler, buttonHandler, size, onChange, addButton = false }) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (onChange) {
+      onChange(event)
+    }
+
+    if (changeHandler) {
+      changeHandler(event.target.value)
+    }
+  }
+
   return (
     <>
       { size === 'small-size'
         ? <>
           <input
             type="text"
+            name={name}
+            value={value}
             className={styles.input}
-            onChange={({ target: { value } }) => changeHandler(value)}
+            onChange={handleChange}
           />
           {
             addButton === true && <button onClick={ buttonHandler }> Mas </button>
@@ -25,8 +40,10 @@ const Input: React.FC<Props> = ({ changeHandler, buttonHandler, size, addButton 
         : <>
           <input
             type="text"
+            name={name}
+            value={value}
             className={`${styles.input} ${styles.medium}`}
-            onChange={({ target: { value } }) => changeHandler(value)}
+            onChange={handleChange}
           />
           {
             addButton === true && <button onClick={ buttonHandler }> Mas </button>
--- src/pages/GameConfig/index.tsx
+++ src/pages/GameConfig/index.tsx
@@ -4,16 +4,18 @@ import Layout from '../../components/templates/Layout'
 import GameConfigForm from '../../components/organisms/GameConfigForm'
 import { paths } from '../../routes'
 
-import { FOOTER_BUTTONS, MOCK_GAME } from './constants'
+import { FOOTER_BUTTONS, INITIAL_STATE_GAME } from './constants'
 import { useHistory } from 'react-router'
 import { addRoom } from '../../firebase/services/room'
+import { useForm } from '../../hooks/useForm'
+import { IRoom } from '../../types/room'
 
 const GameConfig = () => {
-  const data = MOCK_GAME
+  const [values, handleChange,, setValue] = useForm<IRoom>(INITIAL_STATE_GAME as any)
   const history = useHistory()
 
   const handleSubmit = () => {
-    const roomId = addRoom(data)
+    const roomId = addRoom(values)
 
     if (roomId) {
       history.push(paths.PREVIEW.replace(':id', roomId))
@@ -24,10 +26,10 @@ const GameConfig = () => {
     <Layout
       title="Creación de partida"
       subTitle="Configuración de parámetros"
-      closePath={paths.HOME}
+      onClose={() => history.push(paths.HOME)}
       buttons={FOOTER_BUTTONS(handleSubmit)}
     >
-      <GameConfigForm/>
+      <GameConfigForm values={values} handleChange={handleChange} setValue={setValue}/>
     </Layout>
   )
 }
