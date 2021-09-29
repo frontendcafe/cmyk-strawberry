77d73a7e5df69a6a8d3fc584a92425bb4ddba880
Agustin Vazquez <agustinvazquez453@gmail.com>
2021-09-28 21:26:01 -0300

    remove comments

--- src/pages/ProvisionalClasPage.tsx
+++ src/pages/ProvisionalClasPage.tsx
@@ -1,5 +1,4 @@
 import React, { useContext, useEffect, useMemo } from 'react'
-// import styles from './styles/ProvisionalClasPage.module.scss'
 import ClassificationCard from '../components/molecules/ClassificationCard'
 import { Props as ButtonProps } from '../components/atoms/Button'
 import Winner from '../components/molecules/Winner'
@@ -19,61 +18,6 @@ export interface Position {
   score: number,
   image: string // ?
 }
-// const positionArr: Position[] = [
-//   {
-//     id: 1,
-//     name: 'Pepito',
-//     score: 25,
-//     image: 'avatar'
-//   },
-//   {
-//     id: 2,
-//     name: 'Pepito',
-//     score: 35,
-//     image: 'avatar4'
-//   },
-//   {
-//     id: 3,
-//     name: 'Mengano',
-//     score: 45,
-//     image: 'avatar2'
-//   },
-//   {
-//     id: 4,
-//     name: 'Sultano',
-//     score: 18,
-//     image: 'avatar3'
-//   },
-//   {
-//     id: 5,
-//     name: 'Pepito2',
-//     score: 5,
-//     image: 'avatar5'
-//   }
-// ]
-
-// function sortJSON<DataType> (data: DataType[], key:string, orden:'asc' | 'desc') {
-//   return data.sort(function (a:DataType, b:DataType) {
-//     if (key in a && key in b) {
-//       const x = (a as any)[key]
-//       const y = (b as any)[key]
-
-//       if (orden === 'desc') {
-//         return ((x > y) ? -1 : ((x < y) ? 1 : 0))
-//       } else {
-//         return ((x < y) ? -1 : ((x > y) ? 1 : 0))
-//       }
-//     }
-
-//     return -1
-//   })
-// }
-
-// const newPositionJSON = sortJSON(positionArr, 'score', 'desc')
-
-// const winner = newPositionJSON[0]
-
-// const loser = newPositionJSON.filter((_item, index) => index !== 0)
 
 const LABELS = (isEnd: boolean, roundInProgress: number, rounds: number) => isEnd
   ? { title: 'Clasificación Final' }
@@ -107,21 +51,21 @@ const ProvisionalClasPage: React.FC = () => {
   const results = useMemo(() => {
     if (!room?.players) return null
 
-    const winner = isLastRound
+    const winner = !isLastRound
       ? {
         name: room.players[0].name,
         image: room.players[0].imageIndex,
-        score: 21,
+        score: 0,
         isCurrentUser: player.name === room.players[0].name
       }
       : null
 
     return {
       winner,
-      positions: room.players.filter(({ name }) => winner && winner.name !== name).map(({ name, imageIndex }) => ({
+      positions: room.players.filter(({ name }) => winner?.name !== name).map(({ name, imageIndex }) => ({
         name,
         image: imageIndex,
-        score: 20,
+        score: 0,
         isCurrentUser: player.name === name
       }))
     }
@@ -135,9 +79,9 @@ const ProvisionalClasPage: React.FC = () => {
     <Layout
       loading={!results}
       onClose={next}
-      buttons={BUTTONS(isLastRound, next)}
-      isEnded={isLastRound}
-      {...LABELS(isLastRound, room?.roundInProgress, room?.rounds)}
+      buttons={BUTTONS(!isLastRound, next)}
+      isEnded={!isLastRound}
+      {...LABELS(!isLastRound, room?.roundInProgress, room?.rounds)}
     >
       {
         results?.winner
